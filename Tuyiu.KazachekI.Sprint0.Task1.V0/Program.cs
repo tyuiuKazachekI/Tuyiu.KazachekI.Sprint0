fd4fe1b24ae69222d1bf5109829f0523c0f2f3e4
Иван Казачек <tyuiu.KazachekI@outlook.com>
2025-09-13 16:12:52 +0500

    Task1

---
+++ Tuyiu.KazachekI.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuyiu.KazachekI.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            string name = "Иван";
+            int age = 21;
+            Console.WriteLine ("Здраствуйте меня зовут: " + $"{name}" +  " мне:" + $"{age}"+ " год");
+            Console.ReadKey();
+        }
+    }
+}
