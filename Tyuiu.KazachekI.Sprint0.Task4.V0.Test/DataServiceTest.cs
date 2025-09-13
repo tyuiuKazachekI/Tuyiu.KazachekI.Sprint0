9aa3fbbc7df4885c847cda7be9c5982d82958644
Иван Казачек <tyuiu.KazachekI@outlook.com>
2025-09-13 18:17:19 +0500

    Task4

---
+++ Tyuiu.KazachekI.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.KazachekI.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.KazachekI.Sprint0.Task4.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+         public void CheckedAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5,5));
+        }
+        public void CheckedSubtractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        public void CheckedMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+
+        }
+        public void CheckedDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+
+        }
+    }
+        }
+    
+
---
+++ Tyuiu.KazachekI.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KazachekI.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KazachekI.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9862b9d5-0432-4e16-868e-85099d6fe05f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
